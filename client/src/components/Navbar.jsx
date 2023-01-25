fb3051964c50466243935753dcf05030d582f267
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-25 17:11:26 -0500

    Add Login Page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
     }
     return (
         <div className='flex py-4 justify-between items-center'>
-            <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>
+            <span className='flex rounded-lg justify-center items-center w-6 h-6 bg-orange-600 text-xs text-white rounded-sm hover:scale-125' >
                 Barsoom
             </span>
 
--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./features/authSlice";
+import authSlice from "./features/auth/authSlice";
+
 
 const store = configureStore({
   reducer: {
